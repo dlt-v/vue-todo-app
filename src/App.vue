7cb8ff1815478fd7f9e7fb96ec06f539fa14e509
tmm-michalski <tmm.michalski@gmail.com>
2021-07-04 19:44:23 +0200

    Added Sass auto-loader for shared file

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@/assets/_shared.scss';
+
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 
 * {
