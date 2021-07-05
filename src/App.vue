1dbd04692cb83b27de5dbcd3e85b19a98ba85bbb
tmm-michalski <tmm.michalski@gmail.com>
2021-07-05 23:07:52 +0200

    Implement a dummy JSON server for API

--- src/App.vue
+++ src/App.vue
@@ -37,41 +37,67 @@ export default {
             this.showAddTask = !this.showAddTask;
             console.log("?Hello");
         },
-        addTask(newTask) {
-            this.tasks = [...this.tasks, newTask];
+        async addTask(newTask) {
+            const res = await fetch('/api/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(newTask),
+            })
+
+            const data = await res.json()
+
+            this.tasks = [...this.tasks, data]
         },
-        deleteTask(id) {
+        async deleteTask(id) {
             if (confirm("Are you sure?")) {
-                this.tasks = this.tasks.filter(task => task.id !== id);
+
+                const res = await fetch(`/api/tasks/${id}`, {
+                    method: 'DELETE',
+                })
+
+                res.status === 200 ? (this.tasks = this.tasks.filter(task => task.id !== id)) : alert('Error deleting task, try again.');
+
+                
             }
         },
-        toggleReminder(id) {
+        async toggleReminder(id) {
+            const taskToToggle = await this.fetchTask(id);
+            const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+            const res = await fetch(`api/tasks/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(updatedTask)
+            })
+
+            const data = await res.json()
+
             this.tasks = this.tasks.map(task =>
-                task.id === id ? { ...task, reminder: !task.reminder } : task
+                task.id === id ? { ...task, reminder: data.reminder } : task
             );
+        },
+        async fetchTasks() {
+            const res = await fetch('api/tasks')
+
+            const data = await res.json()
+
+            return data;
+            
+        },
+        async fetchTask(id) {
+            const res = await fetch(`api/tasks/${id}`);
+
+            const data = await res.json();
+
+            return data;
         }
     },
-    created() {
-        this.tasks = [
-            {
-                id: 1,
-                text: "Doctors Appointment",
-                day: "March 1st at 2:30pm",
-                reminder: true
-            },
-            {
-                id: 2,
-                text: "Moms birthday",
-                day: "May 15th at 7:30pm",
-                reminder: true
-            },
-            {
-                id: 3,
-                text: "Office Meeting",
-                day: "September 16th at 1:30pm",
-                reminder: false
-            }
-        ];
+    async created() {
+        this.tasks = await this.fetchTasks();
     }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,15 @@ module.exports = {
                 additionalData: '@import "@/assets/_shared.scss";'
             }
         }
-    }
+    },
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                logLevel: 'debug',
+                pathRewrite: { '^/api': '/' },
+          },
+        },
+      },
 };
